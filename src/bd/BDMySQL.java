585d97715704216149614c8f8a3be1d810913dd6
Gabriel Freitas Yamamoto <gabriel@ultronique.com.br>
2020-06-24 15:02:08 -0300

    Correção de erro de escrita

--- src/bd/BDMySQL.java
+++ src/bd/BDMySQL.java
@@ -16,7 +16,7 @@ public class BDMySQL
                       "com.mysql.jdbc.Driver",
                       "jdbc:mysql://localhost:3306/paint?useTimezone=true&serverTimezone=UTC&useSSL=false",
                       BDConfig.user, BDConfig.password); // criar classe BDConfig dentro desse package e setar
-                                                         // user e password como variaveis estáticas lá
+                                                         // user e password como variáveis estáticas lá
         }
         catch (Exception erro)
         {
